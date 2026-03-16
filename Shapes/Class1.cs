a860da727cf7348fcfec244a42773cd5d8650666
Diana Nikulina <djigurdiana@mail.ru>
2026-03-16 23:41:16 +0500

    start

---
+++ Shapes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace Shapes;
+
+public class Point
+{
+    public int X;
+    public int Y;
+
+    public Color Color;
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+}
+
+public class Circle
+{
+    public Point Centre;
+    public int Radious;
+
+    public Color FillColor;
+    public Color OutlineColor;
+
+}
